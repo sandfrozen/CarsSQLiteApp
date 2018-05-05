04697ed86e9f13b3ade9ed9fd745a4a852ac12f6
Tomek Buslowski <tbuslowski@gmail.com>
2018-05-05 14:43:49 +0200

    working with TOP buttons

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import App from './App';
 
 import { YellowBox } from 'react-native';
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
-YellowBox.ignoreWarnings(['Warning: Failed prop type:', 'Module RCTImageLoader']);
-YellowBox.ignoreWarnings(['Module SQLite requires', 'Module RCTImageLoader']);
+YellowBox.ignoreWarnings(['Warning: Failed prop type:']);
+YellowBox.ignoreWarnings(['Module SQLite requires']);
+YellowBox.ignoreWarnings(['Class RCTCxxModule was not exported.']);
 
 AppRegistry.registerComponent('CarsSQLiteApp', () => App);
